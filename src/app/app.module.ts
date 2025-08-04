e102c6f8275009200e0e4b78c594589177bf9e0d
kalema1 <kalemar8@gmail.com>
2025-08-04 20:11:40 +0300

    add the standalone component to the app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [AppComponent],
   bootstrap: [AppComponent],
+  imports: [HeaderComponent, TasksComponent, UserComponent],
 })
 export class AppModule {}
